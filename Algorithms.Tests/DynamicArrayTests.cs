3739136987779f438839a797fcd1ef99ec7bbe86
Tural <tural.mamedov@outlook.com>
2019-05-25 02:19:14 +0300

    Added DynamicArray and Tests

---
+++ Algorithms.Tests/DynamicArrayTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class DynamicArrayTests
+    {
+        private DynamicArray<int> _data;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _data = new DynamicArray<int>(4);
+        }
+
+        [Test]
+        public void GetValue()
+        {
+            _data.Add(1);
+            _data.Add(2);
+            _data.Add(3);
+
+
+            Assert.That(_data[0], Is.EqualTo(1));
+            Assert.That(_data[1], Is.EqualTo(2));
+            Assert.That(_data[2], Is.EqualTo(3));
+        }
+
+        [Test]
+        public void GetValue_WhenIndexIsLowerThanNull_ShouldThrowException()
+        {
+            _data.Add(1);
+            _data.Add(2);
+            _data.Add(3);
+
+            
+            Assert.That(() => _data[-1], Throws.TypeOf<IndexOutOfRangeException>());
+        }
+        
+        [Test]
+        public void SetValue()
+        {
+            _data.Add(1);
+            _data.Add(3);
+            _data.Add(4);
+
+
+            _data[1] = 2;
+
+
+            Assert.That(_data[0], Is.EqualTo(1));
+            Assert.That(_data[1], Is.EqualTo(2));
+            Assert.That(_data[2], Is.EqualTo(3));
+            Assert.That(_data[3], Is.EqualTo(4));
+        }
+
+        [Test]
+        public void SetValue_ShouldIncreaseCapacity()
+        {
+            _data.Add(1);
+            _data.Add(3);
+            _data.Add(4);
+            _data.Add(5);
+
+
+            _data[1] = 2;
+
+
+            Assert.That(_data[0], Is.EqualTo(1));
+            Assert.That(_data[1], Is.EqualTo(2));
+            Assert.That(_data[2], Is.EqualTo(3));
+            Assert.That(_data[3], Is.EqualTo(4));
+            Assert.That(_data[4], Is.EqualTo(5));
+
+            Assert.That(_data.Length, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void SetValue_WhenIndexIsLowerThanNull_ShouldThrowException()
+        {
+            _data.Add(1);
+            _data.Add(2);
+            _data.Add(3);
+
+
+            Assert.That(() => { _data[-1] = 1; }, 
+                Throws.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void Remove()
+        {
+            _data.Add(1);
+            _data.Add(2);
+            _data.Add(3);
+
+
+            _data.Remove(1);
+
+
+            Assert.That(_data[0], Is.EqualTo(1));
+            Assert.That(_data[1], Is.EqualTo(3));
+
+            Assert.That(_data.Length, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Remove_WhenIndexIsLowerThanNull_ShouldThrowException()
+        {
+            Assert.That(() => _data.Remove(-1), 
+                Throws.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void Remove_WhenIndexIsGreaterThanLength_ShouldThrowException()
+        {
+            _data.Add(1);
+
+
+            Assert.That(() => _data.Remove(2),
+                Throws.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void Contains()
+        {
+            _data.Add(1);
+            _data.Add(2);
+            _data.Add(3);
+
+
+            Assert.That(_data.Contains(2), Is.True);
+            Assert.That(_data.Contains(4), Is.False);
+        }
+
+        [Test]
+        public void IsEmpty()
+        {
+            Assert.That(_data.IsEmpty, Is.True);
+        }
+
+        [Test]
+        public void IsEmpty_ShouldReturnFalse()
+        {
+            _data.Add(1);
+            _data.Add(2);
+            _data.Add(3);
+
+
+            Assert.That(_data.IsEmpty, Is.False);
+        }
+    }
+}
---
+++ Algorithms/DynamicArray.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Represents array with dynamic capacity.
+    /// </summary>
+    /// <typeparam name="T">Any type.</typeparam>
+    public class DynamicArray<T>
+    {
+        private int _capacity;
+        private T[] _data;
+        private int _size;
+
+        public int Length
+        {
+            get => _size;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new IndexOutOfRangeException("Index of array cannot be negative value.");
+                }
+                _size = value;
+            }
+        }
+
+        public DynamicArray(int capacity)
+        {
+            _capacity = capacity;
+            _data = new T[capacity];
+        }
+
+        /// <summary>
+        /// Gets value for specified index of array.
+        /// </summary>
+        /// <param name="index">Index of array.</param>
+        /// <exception cref="IndexOutOfRangeException">Index cannot be negative.</exception>
+        /// <returns>Value from specified position.</returns>
+        private T GetValue(int index)
+        {
+            if (index < 0)
+            {
+                throw new IndexOutOfRangeException("Index of array cannot be negative value.");
+            }
+            return _data[index];
+        }
+
+        /// <summary>
+        /// Set relevant value to specified index.
+        /// </summary>
+        /// <param name="index">Number of position in array.</param>
+        /// <param name="value">Value, which needs to be set.</param>
+        private void SetValue(int index, T value)
+        {
+            if (index < 0)
+            {
+                throw new IndexOutOfRangeException("Index of array cannot be negative value.");
+            }
+            if (Length == _capacity)
+            {
+                Resize();
+            }
+            
+            for(int i = Length; i > index; i--)
+            {
+                _data[i] = _data[i - 1];
+            }
+            _data[index] = value;
+            Length++;
+        }
+
+        /// <summary>
+        /// Increases capacity of dynamic array.
+        /// </summary>
+        private void Resize()
+        {
+            T[] newArray = new T[_capacity * 2];
+            for (int i = 0; i < _data.Length; i++)
+            {
+                newArray[i] = _data[i];
+            }
+            _data = newArray;
+            _capacity *= 2;
+        }
+
+        /// <summary>
+        /// Remove value from array in specified index.
+        /// </summary>
+        /// <param name="index">Index of value, which have to be removed.</param>
+        /// <exception cref="IndexOutOfRangeException">Index is negative or index is greater than length.</exception>
+        public void Remove(int index)
+        {
+            if (index < 0)
+            {
+                throw new IndexOutOfRangeException("Index of array cannot be negative value.");
+            }
+
+            if (index > Length)
+            {
+                throw new IndexOutOfRangeException("Specified index greater than length of array.");
+            }
+
+            for (int i = index; i < _data.Length - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            Length -= 1;
+        }
+        
+        /// <summary>
+        /// Checks is there specific element in array.
+        /// </summary>
+        /// <param name="value">Element to check.</param>
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (_data[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks is there any element in array.
+        /// </summary>
+        public bool IsEmpty()
+        {
+            return Length == 0;
+        }
+        
+        /// <summary>
+        /// Adds element to the end of array.
+        /// </summary>
+        public void Add(T value)
+        {
+            if (Length == _capacity)
+            {
+                Resize();
+            }
+
+            _data[Length] = value;
+            Length++;
+        }
+
+        /// <summary>
+        /// Provide access to element by index.
+        /// </summary>
+        /// <param name="key">Index of element.</param>
+        public T this[int key]
+        {
+            get => GetValue(key);
+            set => SetValue(key, value);
+        }
+    }
+}
