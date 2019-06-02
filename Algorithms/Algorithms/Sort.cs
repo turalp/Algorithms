e5187bbf0b0c1e364557db231dcd65d26ab0bbac
Tural <tural.mamedov@outlook.com>
2019-06-02 20:01:33 +0300

    Added sort class, BubbleSort method (not implemented)

---
+++ Algorithms/Algorithms/Sort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorithms.Algorithms
+{
+    /// <summary>
+    /// Provides static methods of sorting algorithms.
+    /// </summary>
+    /// <typeparam name="T">Must implement IComparable.</typeparam>
+    public static class Sort<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// Implementation of bubble sorting.
+        /// </summary>
+        public static void BubbleSort(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[i].CompareTo(arr[j]) == 1)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
