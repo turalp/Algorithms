bb3d1b39d1413c808d6582d0ef1128a13e61e7c0
Tural <tural.mamedov@outlook.com>
2019-06-02 18:32:36 +0300

    Finished implementation of binary heap

--- Algorithms/BinaryHeap.cs
+++ Algorithms/BinaryHeap.cs
@@ -1,10 +1,12 @@
-﻿namespace Algorithms
+﻿using System;
+
+namespace Algorithms
 {
     /// <summary>
     /// Data structure that can find max/min by O(logN).
     /// </summary>
-    /// <typeparam name="T">Any type.</typeparam>
-    public class BinaryHeap<T>
+    /// <typeparam name="T">Type must derive from generic IComparable.</typeparam>
+    public class BinaryHeap<T> where T : IComparable<T>
     {
         private static int _capacity;
         private int _size;
@@ -15,21 +17,45 @@
         /// </summary>
         /// <param name="parentIndex">Index of parent element.</param>
         /// <returns>Index of left child.</returns>
-        private int GetLeftChildIndex(int parentIndex) => 2 * parentIndex + 1;
+        /// <exception cref="IndexOutOfRangeException">Thrown, when one of the indexes is negative or greater than size of array.</exception>
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            if (parentIndex < 0 || parentIndex >= _size)
+            {
+                throw new IndexOutOfRangeException("Index cannot be a negative number or greater than size of binary heap.");
+            }
+            return 2 * parentIndex + 1;
+        }
 
         /// <summary>
         /// Gets right child of binary heap.
         /// </summary>
         /// <param name="parentIndex">Index of parent element.</param>
         /// <returns>Index of right child.</returns>
-        private int GetRightChildIndex(int parentIndex) => 2 * parentIndex + 2;
+        /// <exception cref="IndexOutOfRangeException">Thrown, when one of the indexes is negative or greater than size of array.</exception>
+        private int GetRightChildIndex(int parentIndex)
+        {
+            if (parentIndex < 0 || parentIndex >= _size)
+            {
+                throw new IndexOutOfRangeException("Index cannot be a negative number or greater than size of binary heap.");
+            }
+            return 2 * parentIndex + 2;
+        }
 
         /// <summary>
         /// Gets parent index of binary heap.
         /// </summary>
         /// <param name="childIndex">Index of one of the child element.</param>
         /// <returns>Index of parent element.</returns>
-        private int GetParentIndex(int childIndex) => (childIndex - 1) / 2;
+        /// <exception cref="IndexOutOfRangeException">Thrown, when one of the indexes is negative or greater than size of array.</exception>
+        private int GetParentIndex(int childIndex)
+        {
+            if (childIndex < 0 || childIndex >= _size)
+            {
+                throw new IndexOutOfRangeException("Index cannot be a negative number or greater than size of binary heap.");
+            }
+            return (childIndex - 1) / 2;
+        }
 
         /// <summary>
         /// Checks whether node has left child.
@@ -72,5 +98,115 @@
         /// <param name="index">Index of one of the children.</param>
         /// <returns>Parent of current node.</returns>
         private T Parent(int index) => _data[GetParentIndex(index)];
+
+        /// <summary>
+        /// Inserting value to appropriate position in binary heap.
+        /// </summary>
+        /// <param name="value">Value of node.</param>
+        public void Insert(T value)
+        {
+            if (_size == _capacity)
+            {
+                ResizeCapacity();
+            }
+
+            _data[_size] = value;
+            _size++;
+            HeapifyUp();
+        }
+
+        /// <summary>
+        /// Extracting max value from binary heap.
+        /// </summary>
+        /// <returns>Max value.</returns>
+        /// <exception cref="NotSupportedException">Thrown, when binary heap's size is zero.</exception>
+        public T ExtractMax()
+        {
+            if (_size == 0)
+            {
+                throw new NotSupportedException("Binary heap has no values.");
+            }
+
+            T value = _data[0];
+            _data[0] = _data[_size - 1];
+            _size--;
+            HeapifyDown();
+
+            return value;
+        }
+
+        /// <summary>
+        /// Resize binary heap array with values.
+        /// </summary>
+        public void ResizeCapacity()
+        {
+            T[] newArray = new T[_capacity * 2];
+            for (int i = 0; i < _data.Length; i++)
+            {
+                newArray[i] = _data[i];
+            }
+
+            _data = newArray;
+            _capacity *= 2;
+        }
+        
+        /// <summary>
+        /// Walk upwards to the next node, while parents are less than current value.
+        /// </summary>
+        private void HeapifyUp()
+        {
+            int index = _size - 1;
+            while (HasParent(index) && Parent(index).CompareTo(_data[index]) == -1)
+            {
+                Swap(GetParentIndex(index), index);
+                index = GetParentIndex(index);
+            }
+        }
+
+        /// <summary>
+        /// Walk downwards to the next node, while children are greater than current value.
+        /// </summary>
+        private void HeapifyDown()
+        {
+            int index = 0;
+
+            while (HasLeftChild(index))
+            {
+                int smallerChildIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && RightChild(index).CompareTo(LeftChild(index)) == 1)
+                {
+                    smallerChildIndex = GetRightChildIndex(index);
+                }
+
+                if (_data[index].CompareTo(_data[smallerChildIndex]) == 1)
+                {
+                    break;
+                }
+                else
+                {
+                    Swap(index, smallerChildIndex);
+                }
+
+                index = smallerChildIndex;
+            }
+        }
+
+        /// <summary>
+        /// Swaps a location of nodes by their indexes.
+        /// </summary>
+        /// <param name="fIndex">Index of first candidate to swap.</param>
+        /// <param name="sIndex">Index of second candidate to swap.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown, when one of the indexes is negative or greater than size of array.</exception>
+        private void Swap(int fIndex, int sIndex)
+        {
+            if ((fIndex < 0 || fIndex >= _size) || (sIndex < 0 || sIndex >= _size))
+            {
+                throw new IndexOutOfRangeException("Index cannot be a negative number or greater than size of binary heap.");
+            }
+
+            T value = _data[fIndex];
+            _data[fIndex] = _data[sIndex];
+            _data[sIndex] = value;
+        }
     }
 }
