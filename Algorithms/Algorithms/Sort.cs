3979e2f4ea9759a2c91220e09f8da939d1754806
Tural <tural.mamedov@outlook.com>
2019-06-02 22:49:25 +0300

    Implemented bubble sorting

--- Algorithms/Algorithms/Sort.cs
+++ Algorithms/Algorithms/Sort.cs
@@ -11,18 +11,54 @@ namespace Algorithms.Algorithms
         /// <summary>
         /// Implementation of bubble sorting.
         /// </summary>
-        public static void BubbleSort(T[] arr)
+        /// <param name="arr">Array that needs to be sorted.</param>
+        /// <returns>Sorted array.</returns>
+        /// <exception cref="ArgumentNullException">Thrown, when a reference to array is null.</exception>
+        public static T[] BubbleSort(T[] arr)
         {
-            for (int i = 0; i < arr.Length; i++)
+            if (arr == null)
             {
-                for (int j = i; j < arr.Length; j++)
-                {
-                    if (arr[i].CompareTo(arr[j]) == 1)
-                    {
+                throw new ArgumentNullException(nameof(arr));
+            }
 
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) == 1)
+                    {
+                        T temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
                     }
                 }
             }
+
+            return arr;
+        }
+
+        /// <summary>
+        /// Implementation of merge sorting.
+        /// </summary>
+        /// <param name="arr">Array that needs to be sorted.</param>
+        /// <returns>Sorted array.</returns>
+        /// <exception cref="ArgumentNullException">Thrown, when a reference to array is null.</exception>
+        public static T[] MergeSort(T[] arr)
+        {
+            if (arr == null)
+            {
+                throw new ArgumentNullException(nameof(arr));
+            }
+            
+
+
+            T[] Merge(T left, T middle, T right)
+            {
+                
+                return null;
+            }
+
+            return arr;
         }
     }
 }
