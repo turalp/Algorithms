2f7e557deadf75239d23d85f4e43a24bf21f2e2a
Tural <tural.mamedov@outlook.com>
2019-06-09 12:21:38 +0300

    Graph BFS realization

---
+++ Algorithms.Tests/AlgorithmsTests/GraphTests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Algorithms;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.AlgorithmsTests
+{
+    [TestFixture]
+    public class GraphTests
+    {
+        private Graph _g;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _g = new Graph(8);
+        }
+
+        [Test]
+        public void Bfs()
+        {
+
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Graph.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Algorithms
+{
+    /// <summary>
+    /// Provides graph breadth-first, depth-first search.
+    /// </summary>
+    public class Graph
+    {
+        /// <summary>
+        /// Number of vertices.
+        /// </summary>
+        private int _v;
+
+        /// <summary>
+        /// Adjacency list.
+        /// </summary>
+        private LinkedList<int>[] _adjL;
+
+        public Graph(int v)
+        {
+            _v = v;
+            _adjL = new LinkedList<int>[v];
+            for (int i = 0; i < v; i++)
+            {
+                _adjL[i] = new LinkedList<int>();
+            }
+        }
+
+        /// <summary>
+        /// Adds edge to the instance of graph.
+        /// </summary>
+        /// <param name="v">Number of vertices.</param>
+        /// <param name="w">Value.</param>
+        public void AddEdge(int v, int w)
+        {
+            _adjL[v].AddLast(w);
+        }
+
+        /// <summary>
+        /// Provides breadth-first search for the instance of graph.
+        /// </summary>
+        /// <param name="s">Searched value.</param>
+        public void Bfs(int s)
+        {
+            bool[] visited = new bool[_v];
+
+            Queue<int> queue = new Queue<int>();
+
+            visited[s] = true;
+            queue.Enqueue(s);
+
+            while (queue.Count != 0)
+            {
+                s = queue.Dequeue();
+                IEnumerator<int> ie = _adjL[s].GetEnumerator();
+                while (ie.MoveNext())
+                {
+                    int n = ie.Current;
+                    if (!visited[n])
+                    {
+                        visited[n] = true;
+                        queue.Enqueue(n);
+                    }
+                }
+            }
+        }
+    }
+}
